0dc36a40a221c7801cb3225a11a6dc37b5834716
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-12-03 17:00:35 -0700

    Switch to gradle and setup publishing
    Also fix javadoc errors

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    val indraVer = "3.1.3"
+    id("net.kyori.indra") version indraVer
+    id("net.kyori.indra.publishing") version indraVer
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(libs.org.slf4j.slf4j.api)
+    compileOnly(libs.it.unimi.dsi.fastutil)
+}
+
+tasks.jar {
+    manifest {
+        attributes("FMLModType" to "GAMELIBRARY")
+    }
+}
+
+indra {
+    javaVersions {
+        target(21)
+    }
+    publishSnapshotsTo("paperSnapshots", "https://papermc.io/repo/repository/maven-snapshots/")
+    publishReleasesTo("paperReleases", "https://papermc.io/repo/repository/maven-releases/")
+    gpl3OnlyLicense()
+    github("Tuinity", "ConcurrentUtil")
+    configurePublications {
+        pom {
+            developers {
+                developer {
+                    id = "spottedleaf"
+                }
+            }
+        }
+    }
+}
